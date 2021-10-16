32c387c47a03ad935430b327977a9c60c22be1b0
rahafhamada <Rahooof.1994@gmail.com>
2021-10-16 20:48:32 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import UsersPage  from "./comoponents/users";
 import PostsPage from './comoponents/posts';
-import React from 'react'
+
+
+import React, { useState } from 'react'
+import TapSwitcher from "./comoponents/tapswitcher";
 
 const App = () => {
+  const [activeTab,setActiveTab]= useState("activetab")
+ 
   return (
     <div>
-      <UsersPage/>
-      <PostsPage/>
+    <TapSwitcher  activeTab={activeTab} setActiveTab={setActiveTab}
+    />
+    {activeTab==="users-tab"&& <h1>"user page"</h1>}
+    {activeTab==="posts-tab"&& <h1>"post page"</h1>}
+    {activeTab==="todos-tab"&& <h1>"todos page"</h1>}
+
     </div>
   )
 }
---
+++ src/comoponents/tapswitcher/tapswitcher-style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const TapSwitcherStyle = styled.ul`
+  border-radius: 3px;
+
+  cursor: pointer;
+  color: palevioletred;
+  padding: 0.25em 1em;
+  margin-left: 35%;
+`;
